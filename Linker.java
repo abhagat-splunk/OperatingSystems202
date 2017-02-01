9c9d31b31d34a6aef068adce6bc1b7241661578e
Ankit Bhagat <ankit.bhagat@live.com>
2017-02-01 18:41:13 -0500

    ArrayList of HashMaps for storing overall uses.
    String splitting is a pain. Solve next.

--- Linker.java
+++ Linker.java
@@ -13,7 +13,7 @@ class Codechef
 	    Scanner scanner = new Scanner(System.in);
 		int numberOfModules = scanner.nextInt();
 		int currentBaseAddress = 0;
-		HashMap<String, Integer> definitions = new HashMap<String, Integer>();
+		ArrayList<HashMap<String,List<Integer>>> newUses = new ArrayList<HashMap<String,List<Integer>>>();
 		ArrayList<String> programLines = new ArrayList<String>();
 		HashMap<String, Integer> SymbolTable = new HashMap<String, Integer>();
 		for(int x=0;x<numberOfModules;x++){
@@ -26,7 +26,7 @@ class Codechef
 			    SymbolTable.put(temp1,temp2+currentBaseAddress);
 			}
             int numberOfUses = scanner.nextInt();
-            Map<String, List<Integer>> uses = new HashMap<String, List<Integer>>();
+            HashMap<String, List<Integer>> uses = new HashMap<String, List<Integer>>();
             for(int y=0;y<numberOfUses;y++){
                 String tempKey = scanner.next();
                 if (uses.get(tempKey) == null) {
@@ -38,15 +38,20 @@ class Codechef
                     tempVal = scanner.nextInt();
                 }
                 }
-            System.out.println(uses);
+            if(numberOfUses!=0) 
+                newUses.add(uses);
             int numberOfProgramLines = scanner.nextInt();
             currentBaseAddress+=numberOfProgramLines;
             if(numberOfProgramLines!=0)
                 programLines.add(scanner.nextLine());
-		    
 		}
-	 System.out.println(programLines);	
-	 System.out.println(SymbolTable);   
+	 System.out.println(SymbolTable);
+	 System.out.println(newUses);
+	 for(String s: programLines){
+	     System.out.println(s);
+	     String[] temparr = s.split(" ");
+	     System.out.println(temparr);
+	 }
 	}
 	
 }
